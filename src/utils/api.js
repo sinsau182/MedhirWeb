d03ab2d164320c9382f4e6578cecf257955ce446
sinsau182 <singhsaurav182001@gmail.com>
2025-02-22 16:02:20 +0530

    grpc update and delete done

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,38 +2,6 @@ const address = "/api/superadmin/companies";
 // /api/superadmin/companies
 // http://localhost:3001/companies
 
-export const fetchCompanies = async () => {
-  const res = await fetch(`${address}`);
-  return res.json();
-};
-
-export const createCompany = async (companyData) => {
-  const res = await fetch(`${address}`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(companyData),
-  });
-  return res.json();
-};
-
-export const updateCompany = async (id, updateData) => {
-  const res = await fetch(`${address}`, {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ id, ...updateData }),
-  });
-  return res.json();
-};
-
-export const deleteCompany = async (id) => {
-  const res = await fetch(`${address}`, {
-    method: "DELETE",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ id }),
-  });
-  return res.json();
-};
-
 export const fetchUsers = async () => {
   const res = await fetch("/api/superadmin/user");
   return res.json();
