5ac1f79cf9ed97147ba30d0746ebcb47b289031e
sinsau182 <singhsaurav182001@gmail.com>
2025-03-28 21:25:36 +0530

    ui changes

--- src/components/login-form.jsx
+++ src/components/login-form.jsx
@@ -44,7 +44,7 @@ export function LoginForm({ className, ...props }) {
       if (roles.includes("SUPERADMIN")) {
         router.push("/superadmin/companies");
       } else if (roles.includes("HRADMIN")) {
-        router.push("/hradmin/employees");
+        router.push("/hradmin/dashboard");
       } else {
         router.push("/dashboard"); // Default route
       }
