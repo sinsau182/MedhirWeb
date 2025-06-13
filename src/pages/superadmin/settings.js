dcd4f5cfa293e2caec315508740b30bbc8feef35
sinsau182 <singhsaurav182001@gmail.com>
2025-06-13 19:13:08 +0530

    admin adding solved in module page

--- src/pages/superadmin/settings.js
+++ src/pages/superadmin/settings.js
@@ -1,17 +1,7 @@
-import { useState } from "react";
-import { useRouter } from "next/router";
 import withAuth from "@/components/withAuth";
 import SuperadminHeaders from "@/components/SuperadminHeaders";
 
 function SuperadminSettings() {
-  const router = useRouter();
-  const [activeTab, setActiveTab] = useState("Settings");
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
-  const handleLogout = () => {
-    sessionStorage.removeItem("token");
-    router.push("/login");
-  };
 
   return (
     <div className="bg-white text-black min-h-screen">
