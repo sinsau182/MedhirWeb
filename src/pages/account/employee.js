a44280241f080ec6b3fa4c119616c74341e6b1b8
Megha <megha834004@gmail.com>
2025-07-18 19:02:04 +0530

    employee search bar removed

--- src/pages/account/employee.js
+++ src/pages/account/employee.js
@@ -227,7 +227,17 @@ const Employee = () => {
                 ))}
               </nav>
             </div>
-            <SearchBarWithFilter />
+            
+            <div className="flex justify-between items-center mb-6">
+                          <div className="relative">
+                            <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+                            <input
+                              type="text"
+                              placeholder="Search..."
+                              className="pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent w-64"
+                            />
+                          </div>
+                        </div>
           </div>
           {renderContent()}
         </div>
