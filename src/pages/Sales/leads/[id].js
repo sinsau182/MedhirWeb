a7f6e45553671250887e04c56b099a0564b2c042
sinsau182 <singhsaurav182001@gmail.com>
2025-08-28 13:07:55 +0530

    sales id page corrected for zitadel

--- src/pages/Sales/leads/[id].js
+++ src/pages/Sales/leads/[id].js
@@ -410,8 +410,8 @@ const SalesDetailBody = ({
     ));
   };
 
-  const token = getItemFromSessionStorage("token");
-
+  // const token = getItemFromSessionStorage("token");
+  // const isManager = jwtDecode(token).roles.includes("MANAGER");
   const { userRoles, userModules, isLoading, error } = useUserRolesAndModules();
   const isManager = userRoles.includes("MANAGER");
 
